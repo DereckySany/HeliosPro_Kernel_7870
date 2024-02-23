ea0a4727b699868bcdf52ad7e69fe9b0746929c7
DereckySany <leirecky@gmail.com>
2024-02-23 13:14:32 -0300

    Update download-toolchain.sh

--- download-toolchain.sh
+++ download-toolchain.sh
@@ -17,5 +17,7 @@ tar -xf "$TOOLCHAIN_DIR/arm64-linux-gnu-binaries.tar.xz" -C "$TOOLCHAIN_DIR"
 
 # Adicionar diretório bin ao PATH
 export PATH="$TOOLCHAIN_DIR/bin:$PATH"
+echo "!ist of Toolchain!"
 ls $TOOLCHAIN_DIR
 echo "Toolchain arm64 instalado com sucesso!"
+echo "$PATH"
